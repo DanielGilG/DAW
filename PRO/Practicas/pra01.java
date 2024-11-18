fc51e400f0e8389112a2933cbc78c014c3431f61
Daniel <gilgomezdaniel71@gmail.com>
2024-11-18 22:39:26 +0100

    mod PRO/Practicas/pra01.java

---
+++ PRO/Practicas/pra01.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class pra01 {
+    public static void main(String[] args) {
+        String menu1 = "1.- Películas%n2.- Configuración usuario%n0.- Salir%n-------------------------%nOpción: ";
+        String menu2 = "1.- Casablanca%n2.- El padrino%n0.- Volver%n-------------------------%nOpción: ";
+        String menu3 = "1.- Cambiar nombre usuario%n2.- Cambiar password%n0.- Volver%n-------------------------%nOpción: ";
+        String menu4 = "1.- Administrar películas%n2.- Administrar usuarios%n0.- Salir%n-------------------------%nOpción: ";
+        String menu5 = "1.- Añadir película%n2.- Modificar película%n3.- Borrar película%n0.- Volver%n-------------------------%nOpción: ";
+        String menu6 = "1.- Añadir usuario%n2.- Borrar usuario%n0.- Volver%n-------------------------%nOpción: ";
+
+
+    }
+}
