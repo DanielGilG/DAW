da8920920f5442a9aad11466016cd09ac5bff1bc
Daniel <gilgomezdaniel71@gmail.com>
2024-11-19 19:45:07 +0100

    modifications

--- PRO/Practicas/pra02.java
+++ PRO/Practicas/pra02.java
@@ -153,14 +153,35 @@ public class pra02 {
     }
 
     public void buildMainMenu(int rol, int menu, int opcion){
+        String menu1 = "1.- Películas%n2.- Configuración usuario%n0.- Salir%n-------------------------%nOpción: ";
+        String menu2 = "1.- Casablanca%n2.- El padrino%n0.- Volver%n-------------------------%nOpción: ";
+
+        if (menu == 1) {
+            System.out.printf(menu1);
+        }else{
+            System.out.printf(menu2);
+        }
 
     }
 
-    public void buildUserMenu(int menu, int opcion){
+    public void buildUserMenu(int opcion){
+        String menu3 = "1.- Cambiar nombre usuario%n2.- Cambiar password%n0.- Volver%n-------------------------%nOpción: ";
+
+        switch (opcion) {
+            case 0:
+
+                break;
+        
+            default:
+                break;
+        }
 
     }
 
     public void buildAdminMenu(int menu, int opcion){
+        String menu4 = "1.- Administrar películas%n2.- Administrar usuarios%n0.- Salir%n-------------------------%nOpción: ";
+        String menu5 = "1.- Añadir película%n2.- Modificar película%n3.- Borrar película%n0.- Volver%n-------------------------%nOpción: ";
+        String menu6 = "1.- Añadir usuario%n2.- Borrar usuario%n0.- Volver%n-------------------------%nOpción: ";
 
     }
 }
