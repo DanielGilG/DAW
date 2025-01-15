f367f36d383a35a1fc147b406fb740fa430d3e42
Daniel Gil Gomez <gilgomezdaniel71@gmail.com>
2025-01-15 11:39:22 +0100

    machine TDD

--- PRO/Maven/POO-1/src/test/java/MachineProject/TDD/RotorTest.java
+++ PRO/Maven/POO-1/src/test/java/MachineProject/TDD/RotorTest.java
@@ -1,7 +1,31 @@
 package MachineProject.TDD;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RotorTest {
 
+    @Nested
+    class RotorConstructor{
+        @Test
+        void testEmptyConstructor(){
+            Rotor r1 = new Rotor();
+        }
+
+        @Test
+        void testConstuctorWithConfig(){
+            String[][] config = {{"A", "B", "C", "D"},{"E", "F", "G", "H"}};
+            Rotor r1 = new Rotor(config);
+        }
+
+        @Test
+        void testConstructorsConfig(){
+            String[][] config = {{"A", "B", "C", "D"},{"E", "F", "G", "H"}};
+            Rotor r1 = new Rotor(config);
+            String expectedLetter = "A";
+            assertEquals(expectedLetter, config[0][0]);
+        }
+    }
 }
