5757b5813655c48390ae65321a3a84a15755bd30
Daniel <gilgomezdaniel71@gmail.com>
2025-02-03 21:17:03 +0100

    Automatic push at 2025-02-03 M

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Material de clase
-- Ejercicios
-- Trabajos
+# Content of Web. Application. Development. Course.
+---
+- Java (POO).
+- SQL Queries.
+- HTML/CSS (front-end).
+- *More to come...*
--- SQL_DAW/consultas.sql
+++ SQL_DAW/consultas.sql
@@ -1,10 +1,7 @@
-select c.nombre, c.edad
-from ciclista c
-where c.edad < 30;
+SHOW tables;
 
-select c.nomeq, c.nombre
-from maillot m join llevar l on l.codigo = m.codigo
-			   join etapa e on e.netapa = l.netapa
-			   join ciclista c on c.dorsal = e.dorsal
-where (m.color="Amarillo");
+USE coches;
 
+SELECT * FROM VENTA;
+
+-- \. /home/daniel/Documentos/DAW/SQL_DAW/consultas.sql
