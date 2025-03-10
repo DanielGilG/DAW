fb2497738c34f0f92d3806df72c49207a2dcfeb2
HP Daniel <gilgomezdaniel71@gmail.com>
2025-03-10 12:10:07 +0100

    Automatic uplad at 2025-03-10 P

--- SQL_DAW/DCL/Boletin1DCL.sql
+++ SQL_DAW/DCL/Boletin1DCL.sql
@@ -2,20 +2,36 @@ use jardineria
 
 -- Ejercicio 1
 create user 'admin'@'localhost' identified by 'password'
-grant all privileges *.* to admin;
+grant all privileges on *.* to 'admin'@'localhost';
 
 -- Ejercicio 2
-grant select, insert, update, delete, on jardineria.* to admin;
+grant select, insert, update, delete on jardineria.* to 'admin'@'localhost';
 
 -- Ejercicio 3
 alter user 'admin'@'localhost' identified by 'newpassword';
 
 -- Ejercicio 4
-grant insert, delete, update on jardineria.producto to admin;
+grant alter on jardineria.producto to 'admin'@'localhost';
 
 -- Ejercicio 5
-revoke delete on jardineria.cliente to admin;
+revoke delete on jardineria.producto to 'admin'@'localhost';
 
 -- Ejercicio 6
 create user 'analista'@'localhost' identified by 'securepass';
 grant select on jardineria.pedido to analista;
+
+-- Ejercicio 7
+alter user 'admin'@'localhost' password expire interval 90 day;
+
+-- Ejercicio 8
+grant select(estado), update(estado) on jardineria.pedido to 'admin'@'localhost';
+
+-- Ejercicio 10
+create user 'cliente1'@'localhost' identified by 'clientepass';
+grant select, update on jardineria.pago to 'cliente1'@'localhost'
+where codigo_cliente = current_user();
+
+-- Ejercicio 11
+alter user bblablablabla account lock;
+
+revoke all privileges on jardineria.pago to blablabla;
