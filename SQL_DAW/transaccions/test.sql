9e6f0741e00c5afba6013a9b0cb0c37850907e98
HP Daniel <gilgomezdaniel71@gmail.com>
2025-03-12 13:00:42 +0100

    Automatic uplad at 2025-03-12 P

---
+++ SQL_DAW/transaccions/test.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test CHARACTER SET utf8mb4;
+USE test;
+
+CREATE TABLE cliente (
+    id INT UNSIGNED PRIMARY KEY,
+    nombre CHAR (20)
+);
+
+START TRANSACTION;
+INSERT INTO cliente VALUES (1, 'Pepe');
+COMMIT;
+
+-- 1
+
+SET AUTOCOMMIT=0;
+INSERT INTO cliente VALUES (2, 'Maria');
+INSERT INTO cliente VALUES (20, 'Juan');
+DELETE FROM cliente WHERE nombre = 'Pepe';
+
+-- 2
