e23b70d8d579c0609f415b7ce849f52cda1bdde6
HP Daniel <gilgomezdaniel71@gmail.com>
2025-03-20 13:28:33 +0100

    Automatic uplad at 2025-03-20 P

--- SQL_DAW/transaccions/ejemplo2.sql
+++ SQL_DAW/transaccions/ejemplo2.sql
@@ -10,12 +10,23 @@ CREATE TABLE cliente (
 START TRANSACTION;
 INSERT INTO cliente VALUES (1, 'Pepe');
 COMMIT;
-
--- 1
+/*
+-- 1. ¿Qué devolverá esta consulta?
+SELECT *
+FROM cliente;
 
 SET AUTOCOMMIT=0;
 INSERT INTO cliente VALUES (2, 'Maria');
 INSERT INTO cliente VALUES (20, 'Juan');
 DELETE FROM cliente WHERE nombre = 'Pepe';
 
--- 2
+-- 2. ¿Qué devolverá esta consulta?
+SELECT *
+FROM cliente;
+
+ROLLBACK;
+
+-- 3. ¿Qué devolverá esta consulta?
+SELECT *
+FROM cliente;
+*/
