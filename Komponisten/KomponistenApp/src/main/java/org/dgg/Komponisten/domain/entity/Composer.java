d18a264afb9b86346d716ab0241c70a86363eacb
Daniel <gilgomezdaniel71@gmail.com>
2025-03-21 20:33:00 +0100

    Automatic push at 2025-03-21 M

---
+++ Komponisten/KomponistenApp/src/main/java/org/dgg/Komponisten/domain/entity/Composer.java
@@ -0,0 +1,11 @@
+package org.dgg.Komponisten.domain.entity;
+
+import java.sql.Date;
+
+public class Composer {
+    private int id;
+    private int age;
+    private String name;
+    private String country;
+    private Date birthDate;
+}
