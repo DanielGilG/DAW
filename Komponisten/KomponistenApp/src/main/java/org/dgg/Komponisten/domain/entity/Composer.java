017ab65f6b10e2cdbcf5618d8a3759eec1dd2ad0
Daniel <gilgomezdaniel71@gmail.com>
2025-04-05 18:55:54 +0200

    Added Song and some attributes

--- Komponisten/KomponistenApp/src/main/java/org/dgg/Komponisten/domain/entity/Composer.java
+++ Komponisten/KomponistenApp/src/main/java/org/dgg/Komponisten/domain/entity/Composer.java
@@ -4,13 +4,10 @@ public class Composer {
     private int id;
     private String name;
     private String country;
+    private int yearBorn;
+    private int yearDeath;
 
-    public Composer(){}
-
-    public Composer(int id, String name, String country) {
-        this.id = id;
-        this.name = name;
-        this.country = country;
+    public Composer() {
     }
 
     public Composer(int id, String name) {
@@ -40,5 +37,21 @@ public class Composer {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public int getYearBorn() {
+        return yearBorn;
+    }
+
+    public void setYearBorn(int yearBorn) {
+        this.yearBorn = yearBorn;
+    }
+
+    public int getYearDeath() {
+        return yearDeath;
+    }
+
+    public void setYearDeath(int yearDeath) {
+        this.yearDeath = yearDeath;
     }
 }
---
+++ Komponisten/KomponistenApp/src/main/java/org/dgg/Komponisten/domain/entity/Song.java
@@ -0,0 +1,14 @@
+package org.dgg.Komponisten.domain.entity;
+
+public class Song {
+    private int id;
+    private int composerId;
+    private float duration;
+    private String link;
+    private int yearRelease;
+
+    // Optinal Atributes
+    private int opus; //Op.26
+    private String catalog; //BWV.1003
+    private String songName;
+}
